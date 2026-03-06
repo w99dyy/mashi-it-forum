02c8c631f64a1aa40aa89c229d9a67a0f02036df
Huz <huzskywalker@tutamail.com>
2026-03-06 22:10:12 +0200

    Added thumbnails for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,10 @@ class Post < ApplicationRecord
     has_many_attached :images
     acts_as_taggable_on :tags, :subjects
     has_one_attached :avatar
-    has_one_attached :image
+    has_one_attached :cover_image
 
-    def image_preview
-      image.variant(resize_to_fit: [ 150, 150 ])
+    def thumbnail
+      body.embeds.attachments.first&.blob
     end
 
       scope :by_tag, ->(tag) { tagged_with(tag) if tag.present? }
