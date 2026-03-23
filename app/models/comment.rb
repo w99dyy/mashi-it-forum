ee2af5c7b684969d44ec231aead1399fb048c103
Huz <huzskywalker@tutamail.com>
2026-03-23 02:07:39 +0200

    changed replies name from "comments" to "replies"

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :post, counter_cache: true
   belongs_to :user
+  belongs_to :parent, class_name: "Comment", optional: true
+  has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
   has_many_attached :images
   has_rich_text :body
-  has_one_attached :avatar
 
   validates :body, presence: true
 end
