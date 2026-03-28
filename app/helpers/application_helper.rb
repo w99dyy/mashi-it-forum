5bc1c91cf10db5c73762f6bd80aad8dca98bfe52
Huz <huzskywalker@tutamail.com>
2026-03-28 02:16:21 +0200

    Helper to add conditions for badges

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def user_has_badges?(user, badge_name)
+        return false unless user && user.badges.present?
+
+        user.badges.any? { |badge| badge.name == badge_name }
+    end
 end
