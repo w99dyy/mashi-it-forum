69dcd25a30903a709c54b84e5ce165232a0b1261
Huz <huzskywalker@tutamail.com>
2026-03-30 02:23:43 +0200

    Strip blockquote
    Strip blockquote from replies to prevent nested quoted comments in replies body

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,7 @@
 module CommentsHelper
+  def comment_plain_text(comment)
+    html = comment.body.to_s
+    html_without_quotes = html.gsub(/<blockquote>.*?<\/blockquote>/m, '')
+    ActionController::Base.helpers.strip_tags(html_without_quotes).strip.truncate(300)
+  end
 end
