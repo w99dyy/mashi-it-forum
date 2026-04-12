34e793c9f99f52a8dd20dfa3b7928216ae057c12
Huz <huzskywalker@tutamail.com>
2026-04-13 01:13:07 +0200

    Added: About page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,29 @@
 class PagesController < ApplicationController
   def about
+     @community_stats = {
+      members: User.count,
+      topics: Topic.count,
+      posts: Post.count,
+      admins: User.where(admin: true).count
+    }
+
+    @team_members = [
+      { name: "Weedyy", role: "Forum developer and maintainer", github: "https://github.com/w99dyy" },
+
+      { name: "Serhii", role: "Android app and Discord bot developer" },
+
+      { name: "Atorcran", role: "Mash-it project developer" },
+
+      { name: "Ervindas", role: "Frontend developer" },
+
+      { name: "TurnipDaHeat", role: "Admin" },
+
+      { name: "Potstar1", role: "Admin" },
+
+      { name: "Rylar", role: "Admin" },
+
+      { name: "Kappajoe", role: " Admin" }
+      
+    ]
   end
 end
